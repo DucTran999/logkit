2fdcd0b404ab178ad859aee6e5f035d519d9e343
Đức Trần <tranaduc9x@gmail.com>
2025-07-01 10:46:29 +0700

    test: add unitest for format logger

---
+++ logkit_format_log_test.go
@@ -0,0 +1,121 @@
+package logkit_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DucTran999/logkit"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func Test_Panicf(t *testing.T) {
+	conf := logkit.Config{
+		Environment: logkit.Staging,
+	}
+
+	logInst, err := logkit.NewLogger(conf)
+	if err != nil {
+		t.Fatalf("Init logkit ERR=%v", err)
+	}
+
+	panicOccurred := false
+	defer func() {
+		if r := recover(); r != nil {
+			logInst.Error("example panic log", zap.Any("stack", r))
+			panicOccurred = true
+		}
+		_ = logInst.Sync()
+		require.True(t, panicOccurred, "Expected panic did not occur in Staging environment")
+	}()
+
+	logInst.Panicf("example panic log %v", errors.New("panic test"))
+}
+
+func Test_DPanicInDevelopment(t *testing.T) {
+	conf := logkit.Config{
+		Environment: logkit.Development,
+	}
+
+	logInst, err := logkit.NewLogger(conf)
+	if err != nil {
+		t.Fatalf("Init logkit ERR=%v", err)
+	}
+
+	panicOccurred := false
+	defer func() {
+		if r := recover(); r != nil {
+			logInst.Error("example dpanic log", zap.Any("stack", r))
+			panicOccurred = true
+		}
+		_ = logInst.Sync()
+		require.True(t, panicOccurred, "Expected panic did not occur in Staging environment")
+	}()
+
+	logInst.DPanic("example dpanic log")
+}
+
+func Test_DPanicNotInDevelopment(t *testing.T) {
+	conf := logkit.Config{
+		Environment: logkit.Production,
+	}
+
+	logInst, err := logkit.NewLogger(conf)
+	if err != nil {
+		t.Fatalf("Init logkit ERR=%v", err)
+	}
+
+	panicOccurred := false
+	defer func() {
+		if r := recover(); r != nil {
+			logInst.Error("example dpanic log", zap.Any("stack", r))
+			panicOccurred = true
+		}
+		_ = logInst.Sync()
+		require.False(t, panicOccurred, "Expected panic did not occur in prod environment")
+	}()
+
+	logInst.DPanic("example dpanic log")
+}
+
+func Test_DPanicfInDevelopment(t *testing.T) {
+	conf := logkit.Config{
+		Environment: logkit.Development,
+	}
+
+	logInst, err := logkit.NewLogger(conf)
+	require.NoError(t, err)
+
+	panicOccurred := false
+	defer func() {
+		if r := recover(); r != nil {
+			logInst.Error("example dpanicd log", zap.Any("stack", r))
+			panicOccurred = true
+		}
+		_ = logInst.Sync()
+		require.True(t, panicOccurred, "Expected panic  occur in Development environment")
+	}()
+
+	logInst.DPanicf("example dpanicf log err:%v", errors.New("panic test"))
+}
+
+func Test_DPanicfNotInDevelopment(t *testing.T) {
+	conf := logkit.Config{
+		Environment: logkit.Staging,
+	}
+
+	logInst, err := logkit.NewLogger(conf)
+	require.NoError(t, err)
+
+	panicOccurred := false
+	defer func() {
+		if r := recover(); r != nil {
+			logInst.Error("example dpanicd log", zap.Any("stack", r))
+			panicOccurred = true
+		}
+		_ = logInst.Sync()
+		require.False(t, panicOccurred, "Expected panic not occur in stage environment")
+	}()
+
+	logInst.DPanicf("example dpanicf log err:%v", errors.New("panic test"))
+}
